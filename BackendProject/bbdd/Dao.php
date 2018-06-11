565b1a6e7eae0adc37bd9bde1a871243d10a9a96
a16josortmar <a16josortmar@iam.cat>
2018-06-11 14:03:25 +0200

    Script BBDD + DAO

---
+++ BackendProject/bbdd/Dao.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Josep
+ * Date: 11/06/2018
+ * Time: 11:48
+ */
+    abstract class Dao {
+
+        private $DAO;
+
+        public function __construct () {
+
+            $this->DAO = new mysqli(ConstantsDB::DB_SERVER, ConstantsDB::DB_USER, ConstantsDB::DB_PASSWD, ConstantsDB::DB_NAME);
+
+            if ($this->DAO->connect_errno){
+                echo "Connect failed: " . mysqli_connect_error();
+                exit();
+            }
+
+            $this->DAO->set_charset(ConstantsDB::DB_CHARSET);
+        }
+
+        public function query($sql){
+            $result = $this->DAO->query($sql);
+            return $result;
+        }
+
+        public function getDao(){
+            return $this->DAO;
+        }
+
+        public function close() {
+            return $this->DAO->close();
+        }
+
+        public abstract function findAll();
+
+        public abstract function findById();
+
+        public abstract function findByProperty($property, $value);
+
+        public abstract function save($object);
+
+        public abstract function update($object);
+
+        public abstract function delete($object);
+    }
+
+
+?>
