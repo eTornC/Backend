614b3baeb5cd8f359ea385ee8fcb6d6524ba428e
joseffffff <joseffff@protonmail.com>
2018-06-21 23:14:58 +0200

    config prefix

--- BackendProject/constants/ConstantsDB.php
+++ BackendProject/constants/ConstantsDB.php
@@ -8,6 +8,14 @@ class ConstantsDB {
     const DB_USER   = 'ausias';
     const DB_PASSWD = 'ausias';
 
+    /*
+
+    const DB_NAME   = ' a16josortmar_eTorn';
+    const DB_USER   = ' a16josortmar_et';
+    const DB_PASSWD = 'tF0FDvLeAc';
+
+     */
+
     const DB_CHARSET = 'utf8';
 }
 
--- BackendProject/routes/RouterManager.php
+++ BackendProject/routes/RouterManager.php
@@ -7,29 +7,31 @@ class RouterManager {
 
     public static function manageRoutes(Phroute\Phroute\RouteCollector $router) {
 
+        $prefix = '';
+
         // -----------------------------------------------------------------
         // ---------------------------- STORES -----------------------------
         // -----------------------------------------------------------------
 
-        $router->get('/stores', function () {
+        $router->get($prefix . '/stores', function () {
             return (new StoreManager())->findAll();
         });
 
-        $router->get('/store/{id}', function ($id) {
+        $router->get($prefix . '/store/{id}', function ($id) {
             return (new StoreManager())->findById($id);
         });
 
-        $router->post('/store', function () {
+        $router->post($prefix . '/store', function () {
             $body = file_get_contents('php://input');
             return (new StoreManager())->save(json_decode($body));
         });
 
-        $router->put('/store/{id}', function ($id) {
+        $router->put($prefix . '/store/{id}', function ($id) {
             $body = file_get_contents('php://input');
             return (new StoreManager())->update(json_decode($body), $id);
         });
 
-        $router->delete('/store/{id}', function ($id) {
+        $router->delete($prefix . '/store/{id}', function ($id) {
             return (new StoreManager())->delete($id);
         });
 
@@ -37,25 +39,25 @@ class RouterManager {
         // ---------------------------- QUEUES -----------------------------
         // -----------------------------------------------------------------
 
-        $router->get('/store/{idStore}/queues', function ($idStore) {
+        $router->get($prefix . '/store/{idStore}/queues', function ($idStore) {
             return (new QueueManager())->findAll($idStore);
         });
 
-        $router->get('/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
+        $router->get($prefix . '/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
             return (new QueueManager())->findById($idStore, $idQueue);
         });
 
-        $router->post('/store/{idStore}/queue', function($idStore) {
+        $router->post($prefix . '/store/{idStore}/queue', function ($idStore) {
             $body = file_get_contents('php://input');
             return (new QueueManager())->save(json_decode($body), $idStore);
         });
 
-        $router->put('/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
+        $router->put($prefix . '/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
             $body = file_get_contents('php://input');
             return (new QueueManager())->update(json_decode($body), $idStore, $idQueue);
         });
 
-        $router->delete('/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
+        $router->delete($prefix . '/store/{idStore}/queue/{idQueue}', function ($idStore, $idQueue) {
             return (new QueueManager())->delete($idStore, $idQueue);
         });
 
@@ -63,31 +65,36 @@ class RouterManager {
         // ---------------------------- BUCKETS ----------------------------
         // -----------------------------------------------------------------
 
-        $router->get('/store/{idStore}/queue/{idQueue}/buckets', function ($idStore, $idQueue) {
+        $router->get($prefix . '/store/{idStore}/queue/{idQueue}/buckets', function ($idStore, $idQueue) {
             return '';
         });
 
-        $router->get('/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
+        $router->get($prefix . '/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
             return '';
         });
 
-        $router->post('/store/{idStore}/queue/{idQueue}/bucket', function ($idStore, $idQueue) {
+        $router->post($prefix . '/store/{idStore}/queue/{idQueue}/bucket', function ($idStore, $idQueue) {
             $body = file_get_contents('php://input');
             return '';
         });
 
-        $router->put('/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
+        $router->put($prefix . '/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
             $body = file_get_contents('php://input');
             return '';
         });
 
-        $router->delete('/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
+        $router->delete($prefix . '/store/{idStore}/queue/{idQueue}/bucket/{idBucket}', function ($idStore, $idQueue, $idBucket) {
             return '';
         });
 
         // -----------------------------------------------------------------
         // ---------------------------- TURNS ------------------------------
         // -----------------------------------------------------------------
+
+        $router->get($prefix . '/store/{idStore}/queue/{idQueue}/turns', function ($idStore, $idQueue) {
+            return '';
+        });
+
     }
 
 }
